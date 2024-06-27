568e85a47e18b7defc5d4b1a97112ab91e30a36f
Ariel-Moroshko <45500865+Ariel-Moroshko@users.noreply.github.com>
2024-06-27 10:50:38 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-    <h1>Pokedex with Health.io</h1>
+      <h1>Pokedex with Health.io</h1>
       {/* Add the main app content here - side panel, main view (list vs details) and header */}
     </>
   );
